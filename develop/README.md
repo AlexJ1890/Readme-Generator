e4ee553a20448b85008529b9648b775e297c7613
Alex Hebert <alexjhebert6@gmail.com>
2024-02-01 18:32:11 -0600

    pushing local files 2

---
+++ develop/README.md
@@ -0,0 +1,41 @@
+# Readme Generator
+  
+  ## Description
+  
+    This is designed to allow a user to create a professional readme file using the CLI
+    
+  ## Table of Contents
+  
+    1. [Installation](#installation)
+    2. [Usage](#usage)
+    3. [License](#license)
+    4. [Contribution](#contribution)
+    5. [Testing](#testing)
+  
+  ## Installation
+  
+    N/A
+  
+  ## Usage
+  
+    N/A
+  
+  ## License
+  
+    ![MIT](https://img.shields.io/badge/license-MIT-blue)
+     
+Project is licensed under MIT
+     
+Read more on [this page](https://opensource.org/license/mit/)
+  
+  ## Contribution
+  
+    N/A
+  
+  ## Testing
+  
+    N/A
+  
+  ## Questions
+    Email me at [alexjhebert6@gmail.com](mailto:alexjhebert6@gmail.com) or explore more projects at [AlexJ1890](https://www.github.com/AlexJ1890)
+  
---
+++ develop/index.js
@@ -0,0 +1,69 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const markdown = require("./util/markdown");
+
+const promptUser = () => {
+    return inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is your Github user name?',
+      name: 'github',
+    },
+    {
+      type: 'input',
+      message: 'What is your email address?',
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'project',
+    },
+    {
+        type: 'input',
+        message: 'Describe your project',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'Enter usage information',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+        name: 'license',
+      },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions?',
+        name: 'test',
+    },
+    
+  ])
+}
+
+
+
+
+const init = () => {
+    promptUser()
+   
+      .then((answers) => fs.writeFile('README.md', markdown({...answers}), (err)=> console.log(err))) 
+      .then(() => console.log('Successfully created README'))
+      .catch((err) => console.error(err));
+  };
+
+
+init();
